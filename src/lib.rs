3442629afde4efa34b991a7809405e16e454775a
RunnerScrab <RunnerScrab@nospam.spam>
2026-04-06 17:53:37 -0500

    Use new approx_ln()

--- src/lib.rs
+++ src/lib.rs
@@ -83,7 +83,7 @@ impl WyRand {
         let rv: u64 = self.next_u64();
         let u1 = 1.0 - self.next_f32();
         let u2 = 1.0 - self.next_f32();
-        let r = (-fast_mul2_f32(u1.ln())).approx_sqrt();
+        let r = (-fast_mul2_f32(u1.approx_ln())).approx_sqrt();
         //r * (Self::TWO_PI_F32 * u1).sin()
         r * (Self::TWO_PI_F32 * u2).approx_cos()
     }
@@ -94,7 +94,7 @@ impl WyRand {
         // Box-Muller uses rvs in (0, 1]; subtracting a rv on [0, 1) from 1 gives an rv in (0, 1]
         let u1 = 1.0 - self.next_f64();
         let u2 = 1.0 - self.next_f64();
-        let r = (-fast_mul2_f64(u1.ln())).approx_sqrt();
+        let r = (-fast_mul2_f64(u1.approx_ln())).approx_sqrt();
         //r * (Self::TWO_PI_F64 * u1).sin()
         r * (Self::TWO_PI_F64 * u2).approx_cos()
     }
